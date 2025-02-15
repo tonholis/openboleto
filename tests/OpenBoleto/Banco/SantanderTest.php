d3e09a88c6b86da0d73fa13f87e75ba0dc7cd49a
Cleyton Tonholi <tonholis@gmail.com>
2025-02-15 12:16:42 +0100

    reset to fator de vencimento

--- tests/OpenBoleto/Banco/SantanderTest.php
+++ tests/OpenBoleto/Banco/SantanderTest.php
@@ -29,4 +29,70 @@ class SantanderTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('03399.12347 56700.123450 67890.101024 6 55650000002300', $instance->getLinhaDigitavel());
         $this->assertSame('0012345678901', (string) $instance->getNossoNumero());
     }
+
+    protected function getFatorVencimento($linhaDigitavel)
+    {
+        $parts = explode(' ', $linhaDigitavel);
+        return substr($parts[4], 0, 4);
+    }
+
+    public function testFatorVencimentoLimite2025()
+    {
+        $instance = new Santander(array(
+            // Parâmetros obrigatórios
+            'dataVencimento' => new \DateTime('2025-02-21'),
+            'valor' => 1.00,
+            'sequencial' => 12345678901, // Até 13 dígitos
+            'agencia' => 1234, // Até 4 dígitos
+            'carteira' => 102, // 101, 102 ou 201
+            'conta' => 1234567, // Código do cedente: Até 7 dígitos
+            // IOS – Seguradoras (Se 7% informar 7. Limitado a 9%)
+            // Demais clientes usar 0 (zero)
+            'ios' => '0', // Apenas para o Santander
+        ));
+
+        $this->assertInstanceOf('OpenBoleto\\Banco\\Santander', $instance);
+
+        $this->assertEquals('9999', $this->getFatorVencimento($instance->getLinhaDigitavel()));
+    }
+
+    public function testFatorVencimentoReset2025()
+    {
+        $instance = new Santander(array(
+            // Parâmetros obrigatórios
+            'dataVencimento' => new \DateTime('2025-02-22'),
+            'valor' => 1.00,
+            'sequencial' => 12345678901, // Até 13 dígitos
+            'agencia' => 1234, // Até 4 dígitos
+            'carteira' => 102, // 101, 102 ou 201
+            'conta' => 1234567, // Código do cedente: Até 7 dígitos
+            // IOS – Seguradoras (Se 7% informar 7. Limitado a 9%)
+            // Demais clientes usar 0 (zero)
+            'ios' => '0', // Apenas para o Santander
+        ));
+
+        $this->assertInstanceOf('OpenBoleto\\Banco\\Santander', $instance);
+        $this->assertEquals('1000', $this->getFatorVencimento($instance->getLinhaDigitavel()));
+    }
+
+    public function testFatorVencimentoPrimeiro2025()
+    {
+        $instance = new Santander(array(
+            // Parâmetros obrigatórios
+            'dataVencimento' => new \DateTime('2025-02-23'),
+            'valor' => 1.00,
+            'sequencial' => 12345678901, // Até 13 dígitos
+            'agencia' => 1234, // Até 4 dígitos
+            'carteira' => 102, // 101, 102 ou 201
+            'conta' => 1234567, // Código do cedente: Até 7 dígitos
+            // IOS – Seguradoras (Se 7% informar 7. Limitado a 9%)
+            // Demais clientes usar 0 (zero)
+            'ios' => '0', // Apenas para o Santander
+        ));
+
+        $this->assertInstanceOf('OpenBoleto\\Banco\\Santander', $instance);
+
+        $parts = explode(' ', $instance->getLinhaDigitavel());
+        $this->assertEquals('1001', $this->getFatorVencimento($instance->getLinhaDigitavel()));
+    }
 }
